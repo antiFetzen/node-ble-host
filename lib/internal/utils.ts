63dee176510e85a67e39ed33a61c020ada41c29d
Christoph Ehrensperger <christoph.ehrensperger@patrizia.ag>
2022-11-29 21:34:58 +0100

    Translate   - utils.js -> utils.ts   - hci-errors.ji -> hci-errors.ts
      Create BleAddressTypes

---
+++ lib/internal/utils.ts
@@ -0,0 +1,188 @@
+import { EventEmitter } from 'events'
+
+const BASE_UUID_SECOND_PART = '-0000-1000-8000-00805F9B34FB'
+
+
+export class Queue<T> {
+	q: T[]
+	pos: number
+
+	constructor() {
+		this.q = []
+		this.pos = 0
+	}
+
+	getLength(): number {
+		return this.q.length - this.pos
+	}
+
+	push(item: T): void {
+		this.q.push(item)
+	}
+
+	shift(): T {
+		if (this.pos === this.q.length) return undefined
+
+		const elem = this.q[this.pos++]
+
+		if (this.pos * 2 >= this.q.length) {
+			this.q.slice(0, this.pos)
+			this.pos = 0
+		}
+
+		return elem
+	}
+
+	peek(): T | undefined {
+		return this.pos === this.q.length ? undefined : this.q[this.pos]
+	}
+
+	getAt(i: number): T | undefined {
+		return this.pos + i >= this.q.length ? undefined : this.q[this.pos + 1]
+	}
+}
+
+export class IdGenerator {
+	last: string
+
+	constructor() {
+		this.last = ''
+	}
+
+	next(): string {
+		for (let pos = this.last.length - 1; pos >= 0; --pos) {
+			if (this.last[pos] !== 'z') {
+				return this.last = this.last.substr(0, pos) + String.fromCharCode(this.last.charCodeAt(pos) + 1) + 'a'.repeat(this.last.length - pos - 1)
+			}
+		}
+
+		return this.last = 'a'.repeat(this.last.length + 1)
+	}
+}
+
+export interface CacheNode {
+	key: string
+	value: any
+	next: CacheNode | null
+	prev: CacheNode | null
+}
+
+export class DuplicateCache extends EventEmitter {
+	first: CacheNode | null
+	last: CacheNode | null
+	nodeMap: { [key: string]: CacheNode }
+	capacity: number
+
+	dc: DuplicateCache
+
+	constructor(capacity: number) {
+		super()
+
+		if (capacity <= 0) throw new Error('Invalid capacity')
+		this.capacity = capacity
+
+		this.first = null
+		this.last = null
+		this.nodeMap = Object.create(null)
+
+		this.dc = this
+	}
+
+	isDuplicate(key: string): boolean {
+		return key in this.nodeMap
+	}
+
+	get(key: string): any {
+		return this.isDuplicate(key) ? this.nodeMap[key].value : null
+	}
+
+	remove(key: string): boolean {
+		if (this.isDuplicate(key)) {
+			const node = this.get(key)
+			delete this.nodeMap[key]
+
+			if (node.next !== null) node.next.prev = node.prev
+
+			if (this.first === node) this.first = node.next
+
+			if (this.last === node) this.last = node.prev
+
+			++this.capacity
+
+			return true
+		}
+
+		return false
+	}
+
+	add(key: string, value: any): boolean {
+		const exists = this.dc.remove(key)
+
+		let firstKey, removedFirstKey = false
+		if (this.capacity === 0) {
+			firstKey = this.first.key
+			removedFirstKey = true
+			delete this.nodeMap[this.first.key]
+			this.first = this.first.next
+			if (this.first) {
+				this.first.prev = null
+			} else {
+				this.last = null
+			}
+
+			++this.capacity
+		}
+
+		const node: CacheNode = {
+			key,
+			value,
+			next: null,
+			prev: this.last,
+		}
+
+		this.nodeMap[key] = node
+		this.last = node
+
+		--this.capacity
+		if (removedFirstKey && firstKey !== key) this.dc.emit('remove', firstKey)
+
+		return !exists
+	}
+}
+
+export function serializeUuid(uuid: string | number): Buffer {
+	if (typeof uuid === 'string') {
+		if (uuid.substr(8) === BASE_UUID_SECOND_PART && uuid.substr(0, 4) === '0000') {
+			return Buffer.from(uuid.substr(4, 4), 'hex').reverse()
+		}
+		const ret = Buffer.from(uuid.replace(/-/g, ''), 'hex').reverse()
+		if (ret.length === 16) {
+			return ret
+		}
+	} else if (Number.isInteger(uuid) && uuid >= 0 && uuid <= 0xffff) {
+		return Buffer.from([uuid, uuid > 8])
+	}
+
+	throw new Error('Invalid uuid: ' + uuid)
+}
+
+export function isValidBdAddr(bdAddr: string): boolean {
+	return typeof bdAddr === 'string' && /^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$/.test(bdAddr)
+}
+
+export function bdAddrToBuffer(address: string): Buffer {
+	const buf = []
+	for (let i = 15; i >= 0; i -= 3) {
+		buf.push(parseInt(address.substr(i, 2), 16))
+	}
+	return Buffer.from(buf)
+}
+
+export default {
+	Queue,
+	IdGenerator,
+	DuplicateCache,
+	serializeUuid,
+	isValidBdAddr,
+	bdAddrToBuffer,
+}
---
+++ types/BleAddressTypes.ts
@@ -0,0 +1,9 @@
+export enum BleAddressTypes {
+  PUBLIC= 'public',
+  RANDOM = 'random',
+}
+
+export enum BleAddressTypesValue {
+  PUBLIC_VALUE = 0,
+  RANDOM_VALUE = 1,
+}
